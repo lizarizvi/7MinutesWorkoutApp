3644fe8b9ce1840ddd9c0921ff4236ce51d2a424
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-12 18:28:45 +0530

    setting exercises in Constants.kt

---
+++ app/src/main/java/com/example/a7minutesworkoutapp/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.a7minutesworkoutapp
+
+object Constants {
+
+    fun defaultExerciseList() : ArrayList<ExerciseModel>{
+        var exerciseList = ArrayList<ExerciseModel>()
+
+
+
+        return exerciseList
+    }
+}
