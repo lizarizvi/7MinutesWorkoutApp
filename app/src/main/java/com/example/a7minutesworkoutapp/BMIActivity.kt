cb1e8feb7f365de2eca4094621890d28dec438bb
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-17 13:53:00 +0530

    validateMetricUnits function

--- app/src/main/java/com/example/a7minutesworkoutapp/BMIActivity.kt
+++ app/src/main/java/com/example/a7minutesworkoutapp/BMIActivity.kt
@@ -2,6 +2,7 @@ package com.example.a7minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import com.example.a7minutesworkoutapp.databinding.ActivityBmiBinding
 
@@ -24,5 +25,26 @@ class BMIActivity : AppCompatActivity() {
         binding?.toolbarBMI?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        binding?.btnBMI?.setOnClickListener {
+            if (validateMetricUnits()){
+                val heightValue: Float = binding?.etHeight?.text.toString().toFloat() / 100
+                val weightValue: Float = binding?.etWeight?.text.toString().toFloat()
+                val bmi = weightValue / (heightValue * heightValue)
+                //TODO display the result
+            }else{
+                Toast.makeText(this@BMIActivity,"Please enter valid values.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun validateMetricUnits(): Boolean{
+        var isValid = true
+        if(binding?.etWeight?.text.toString().isEmpty()){
+            isValid = false
+        }else if (binding?.etHeight?.text.toString().isEmpty()){
+            isValid = false
+        }
+        return isValid
     }
 }
