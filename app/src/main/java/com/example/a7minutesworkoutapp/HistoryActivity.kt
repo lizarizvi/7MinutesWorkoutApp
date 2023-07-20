6fb9d16f298d16fe04681dce55e87dca09ca4d9e
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-20 17:11:35 +0530

    retrieving the data from database and displaying in logcat

--- app/src/main/java/com/example/a7minutesworkoutapp/HistoryActivity.kt
+++ app/src/main/java/com/example/a7minutesworkoutapp/HistoryActivity.kt
@@ -2,8 +2,12 @@ package com.example.a7minutesworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.a7minutesworkoutapp.databinding.ActivityExcerciseBinding
 import com.example.a7minutesworkoutapp.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -22,5 +26,26 @@ class HistoryActivity : AppCompatActivity() {
         binding?.toolbarHistory?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        val historyDao = (application as WorkoutApp).db.historyDao()
+        getAllDates(historyDao)
+
     }
+
+    private fun getAllDates(historyDao: HistoryDao){
+        lifecycleScope.launch {
+            historyDao.fetchAllDates().collect{
+                allDatesList->
+                for (i in allDatesList){
+                    Log.e("Date: ",""+i)
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
 }
